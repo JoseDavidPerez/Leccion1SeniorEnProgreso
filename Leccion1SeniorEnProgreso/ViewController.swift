a5adca3f679093367c2223776a8abb06daca7f43
davoaxolt <dp3147850@gmail.com>
2024-01-15 19:22:13 -0600

    first commit

---
+++ Leccion1SeniorEnProgreso/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leccion1SeniorEnProgreso
+//
+//  Created by Jose David Torres Perez on 15/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
